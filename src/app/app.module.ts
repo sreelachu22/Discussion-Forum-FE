222acdaf8a520b65a6f3edde2a827d7c4adfd454
Sreelakshmi P M <sree2000lachu@gmail.com>
2024-01-19 18:20:20 +0530

    feat:- adding some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { IconComponent } from './icon/icon.component';
-import { SidenavComponent } from './sidenav/sidenav.component';
+import { IconComponent } from './components/ui/icon/icon.component';
+import { SidenavComponent } from './components/layout/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    IconComponent,
-    SidenavComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, IconComponent, SidenavComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
