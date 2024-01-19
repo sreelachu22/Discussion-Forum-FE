7e4d43106445362c46019f3727024d2307473d3e
Sreelakshmi P M <sree2000lachu@gmail.com>
2024-01-19 18:17:41 +0530

    feat:- adding some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IconComponent } from './icon/icon.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
