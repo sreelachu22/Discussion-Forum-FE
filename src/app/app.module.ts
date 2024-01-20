d638ddd1be6356cbc2913db68aa72c3a4456fb5c
merinjose <merin.jose@experionglobal.com>
2024-01-20 14:51:14 +0530

    feat:- header component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconComponent } from './components/ui/icon/icon.component';
 import { SidenavComponent } from './components/layout/sidenav/sidenav.component';
+import { HeaderComponent } from './components/layout/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, IconComponent, SidenavComponent],
+  declarations: [AppComponent, IconComponent, SidenavComponent, HeaderComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
