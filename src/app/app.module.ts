3b4cf84b2074b4d8cc4b74fd17e7d1d0b48e3170
merinjose <merin.jose@experionglobal.com>
2024-01-22 20:09:42 +0530

    feat: notices page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,23 @@ import { HeaderComponent } from './components/layout/header/header.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardComponent } from './components/ui/card/card.component';
+import { NoticesComponent } from './features/community_head/notices/notices.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @NgModule({
-  declarations: [AppComponent, IconComponent, SidenavComponent, CardComponent,HeaderComponent],
+  declarations: [AppComponent, IconComponent, SidenavComponent, CardComponent,HeaderComponent, NoticesComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FontAwesomeModule,
     BrowserAnimationsModule,
-    
+    HttpClientModule,
+    FormsModule,
+    ModalModule.forRoot(),
+    BsDatepickerModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent],
