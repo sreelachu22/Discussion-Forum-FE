6dfac46d12f052f727ea17e22bc74a1cd120387b
merinjose <merin.jose@experionglobal.com>
2024-02-03 09:56:25 +0530

    feat:- reusable list and card components added and used

--- src/app/components/ui/list/list.component.ts
+++ src/app/components/ui/list/list.component.ts
@@ -7,14 +7,15 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
-  @Input() createdBy: string = '';
-  @Input() status: string = '';
-  @Input() createdOn: string = '';
-  @Input() post: string = '';
-  @Input() votes: number = 0;
-  @Input() tags: string = '';
-  @Input() isAnswered: boolean = false;
-
+  // @Input() createdBy: string = '';
+  // @Input() status: string = '';
+  // @Input() createdOn: string = '';
+  // @Input() post: string = '';
+  // @Input() votes: number = 0;
+  // @Input() tags: string = '';
+  // @Input() isAnswered: boolean = false;
+  // @Input() metadataItems: { name: string; value: any }[] = [];
+  @Input() sections: { title: string; metadataItems: { name: string; value: any }[] }[] = [];
   @Input() click!: () => void;
 
   onClick() {
