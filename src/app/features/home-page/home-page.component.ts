3edcffd83be9f7e4a17ddf1fd7966e54e27a5d15
Abraham Jacob Kurialanickal <abraham.kurialanickal@experionglobal.com>
2024-02-07 06:14:26 +0530

    feat:- populated community details

--- src/app/features/home-page/home-page.component.ts
+++ src/app/features/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { CommunityDetails, CommunityService } from 'src/app/service/HttpServices/community.service';
 
 @Component({
   selector: 'app-home-page',
@@ -7,15 +8,30 @@ import { ActivatedRoute, Router } from '@angular/router';
   styleUrls: ['./home-page.component.css'],
 })
 export class HomePageComponent {
-  constructor(private activateRoute: ActivatedRoute, private router: Router) {}
-  communityID: number = 1;
+  constructor(
+    private httpService: CommunityService,
+    private activateRoute: ActivatedRoute, 
+    private router: Router
+  ) {}
 
-  ngOnInit() {}
+  communities : CommunityDetails[] = [];
+  ngOnInit():void {
+    this.loadCommunities();
+  }
 
-  navigateToCommunity() {
+  loadCommunities() {
+    this.httpService
+    .getAllCommunities()
+    .subscribe((data) => {
+      console.log(data);
+    this.communities = data;
+  });
+  }
+
+  navigateToCommunity(communityId: number) {
     this.router.navigate(['community'], {
       queryParams: {
-        communityID: this.communityID,
+        communityID: communityId,
       },
     });
   }
--- src/app/service/HttpServices/community.service.ts
+++ src/app/service/HttpServices/community.service.ts
@@ -2,13 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+export interface CommunityDetails{
+  communityId : number;
+  communityName : string;
+  communityStatusName : string;
+  createdBy : string;
+  createdAt : Date;
+  modifiedBy : string | null;
+  modifiedAt : Date | null;
+  categoryCount : number | null;
+  postCount : number | null;
+  topCategories : string [] | null;
+}
 @Injectable({
   providedIn: 'root',
 })
 export class CommunityService {
   constructor(private http: HttpClient) {}
 
-  getCommunities(): Observable<any> {
-    return this.http.get(`https://localhost:7160/api/Community`);
+  BASE_URL = 'https://localhost:7160/api/Community'
+
+  getAllCommunities(): Observable<any> {
+    return this.http.get<any>(this.BASE_URL);
+  }
+
+  getACommunity(id: number): Observable<CommunityDetails> {
+    return this.http.get<CommunityDetails>(`${this.BASE_URL}/${id}`);
   }
 }
