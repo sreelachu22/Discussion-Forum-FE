fe104e3c8168131988085525e982f2c73f257cd9
Alvin Ragi <alvin.ragi@experionglobal.local>
2024-02-14 14:15:06 +0530

    edited user mgmt, notifications

---
+++ src/app/components/ui/header-search/header-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header-search',
+  templateUrl: './header-search.component.html',
+  styleUrls: ['./header-search.component.css']
+})
+export class HeaderSearchComponent {
+  searchText: string = '';
+  @Output() searchClicked: EventEmitter<string> = new EventEmitter<string>();
+
+  onSearch() {
+    this.searchClicked.emit(this.searchText);
+  }
+}
