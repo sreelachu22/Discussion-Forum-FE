c346415d5f2ea817b6bc84bf27ae6b7a0af4950e
Alvin Ragi <alvin.ragi@experionglobal.local>
2024-03-14 12:53:10 +0530

    ui changes

--- src/app/service/HttpServices/tag.service.ts
+++ src/app/service/HttpServices/tag.service.ts
@@ -9,16 +9,22 @@ import { environment } from 'src/app/environments/environment';
 export class TagService {
   constructor(private http: HttpClient) {}
   apiurl: string = environment.apiUrl;
-  sortOrder:string = "asc";
-  pageNumber:number = 1;
+  sortOrder: string = 'asc';
+  pageNumber: number = 1;
 
   getAllTags(): Observable<any> {
     return this.http.get(this.apiurl + `Tag?true`);
   }
 
-  getTagsWithSearchAndSort(inputSortOrder:string, inputPageNumber:number):Observable<any>{
+  getTagsWithSearchAndSort(
+    inputSortOrder: string,
+    inputPageNumber: number
+  ): Observable<any> {
     this.sortOrder = inputSortOrder;
     this.pageNumber = inputPageNumber;
-    return this.http.get(this.apiurl+ `Tag/PaginatedTagsWithSearchAndSort?isdel=false&sortOrder=${this.sortOrder}&pageNumber=${this.pageNumber}&pageSize=15`)
+    return this.http.get(
+      this.apiurl +
+        `Tag/PaginatedTagsWithSearchAndSort?isdel=false&sortOrder=${this.sortOrder}&pageNumber=${this.pageNumber}&pageSize=12`
+    );
   }
 }
